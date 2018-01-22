8aa0806ec3241e912e52cce9bd0b2f1075faff4c
katudd <katrin@uddstromer.se>
2018-01-22 11:56:32 +0100

    Backend set up

---
+++ server.js
@@ -0,0 +1,10 @@
+import express from "express"
+// import mongoose from "mongoose"
+
+const app = express()
+
+app.get("/", (req, res) =>
+  res.send("Hello World!"))
+
+app.listen(8080, () =>
+  console.log("Example app listening on port 8080!"))
