6046f2f969d5cc81ce7312a34f06d8dd6c8010ca
katudd <katrin@uddstromer.se>
2018-01-22 14:03:40 +0100

    Export Adminpage

--- frontend/src/Admin/admin-page.js
+++ frontend/src/Admin/admin-page.js
@@ -2,7 +2,7 @@ import React from "react"
 
 import "./style.css"
 
-class Adminpage extends React.Component {
+export default class Adminpage extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
