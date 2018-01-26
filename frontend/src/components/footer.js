6c563829442a73fbdaf0571c399134ad099d633d
Katarina Sjölund <katarina.sjolund@outlook.com>
2018-01-26 11:55:55 +0100

    changed path on style for footer och nav

--- frontend/src/components/footer.js
+++ frontend/src/components/footer.js
@@ -1,7 +1,7 @@
 import React from "react"
 import { Link } from "react-router-dom"
 
-import "../pages/startpage/style.css"
+import "pages/start/style.css"
 
 export default class Footer extends React.Component {
   render() {
