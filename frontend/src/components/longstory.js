06106d7009ab0ade07298acc61fba90ca434df73
Katarina Sjölund <katarina.sjolund@outlook.com>
2018-01-30 14:24:53 +0100

    longstory css

--- frontend/src/components/longstory.js
+++ frontend/src/components/longstory.js
@@ -1,13 +1,16 @@
 import React from "react"
 import ReactMarkdown from "react-markdown"
 
-import "../pages/story/style.css"
+// import "../pages/story/style.css"
+import "./longstory.css"
 
 export default class Longstory extends React.Component {
   render() {
     return (
       <div className="longstory-container">
-        <img src={this.props.image} alt="" />
+        <div className="longstory-image-container">
+          <img id="longstory-image" src={this.props.image} alt="" />
+        </div>
         <p className="caption">{this.props.caption}</p>
         <h2 className="heading">{this.props.heading}</h2>
         <div className="longtext"><ReactMarkdown source={this.props.longText} /></div>
